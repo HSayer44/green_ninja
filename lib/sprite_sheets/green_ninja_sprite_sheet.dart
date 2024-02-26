031231bf51285591317c7123d6e55bb180659fa6
Al Sayer <alsayerhaitham@gmail.com>
2024-02-26 14:37:58 +0100

    init

---
+++ lib/sprite_sheets/green_ninja_sprite_sheet.dart
@@ -0,0 +1,16 @@
+import 'package:bonfire/bonfire.dart';
+
+import '../constants/globals.dart';
+
+class GreenNinjaSpriteSheet {
+  static Future<void> load() async {
+    final image = await Flame.images.load(Globals.greenNinjaSpriteSheet);
+    spriteSheet = SpriteSheet.fromColumnsAndRows(
+      image: image,
+      columns: 4,
+      rows: 7,
+    );
+  }
+
+  static late SpriteSheet spriteSheet;
+}
---
+++ lib/sprite_sheets/old_man_sprite_sheet.dart
@@ -0,0 +1,16 @@
+import 'package:bonfire/bonfire.dart';
+
+import '../constants/globals.dart';
+
+class OldManSpriteSheet {
+  static Future<void> load() async {
+    final image = await Flame.images.load(Globals.oldManSpriteSheet);
+    spriteSheet = SpriteSheet.fromColumnsAndRows(
+      image: image,
+      columns: 4,
+      rows: 7,
+    );
+  }
+
+  static late SpriteSheet spriteSheet;
+}
