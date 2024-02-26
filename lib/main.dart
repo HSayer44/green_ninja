7431e65508d5f99b7b568a40d954aaec6b33f936
Al Sayer <alsayerhaitham@gmail.com>
2024-02-26 14:45:32 +0100

    init

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,37 @@
+import 'package:flame_audio/flame_audio.dart';
 import 'package:flutter/material.dart';
+import 'package:green_ninja/constants/globals.dart';
+import 'package:green_ninja/sprite_sheets/blue_ninja_sprite_sheet.dart';
+import 'package:green_ninja/sprite_sheets/dark_ninja_sprite_sheet.dart';
+import 'package:green_ninja/sprite_sheets/green_ninja_sprite_sheet.dart';
+import 'package:green_ninja/sprite_sheets/old_man_sprite_sheet.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GreenNinjaSpriteSheet.load();
+  await BlueNinjaSpriteSheet.load();
+  await OldManSpriteSheet.load();
+  await DarkNinjaSpriteSheet.load();
+
+  await FlameAudio.audioCache.loadAll([
+    Globals.gameOverSound,
+    Globals.powerUpSound,
+    Globals.successSound,
+    Globals.explosionSound,
+    Globals.fireSound,
+  ]);
+
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
